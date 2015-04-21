981a77bc47f5186256d599bf43f6299826b43195
Petro Koriakin <pkor@softserveinc.com>
2015-04-22 00:17:04 +0300

    commit #1

--- some_code.rb
+++ some_code.rb
@@ -2,7 +2,7 @@ puts 'Hello worldsr'
 
 puts 23+89
 
-puts 'git checkout -b new_branch_name'
-puts 'git push -u origin new_branch_name'
+puts 'git checkout -b new_branch'
+puts 'git push -u origin new_branch'
 
 puts 'one one one'
